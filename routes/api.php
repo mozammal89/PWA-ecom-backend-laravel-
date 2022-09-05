80cd0da05bd008fa3dc680360b359146869cb303
mozammal89 <mozammal.bubt89@gmail.com>
2022-09-06 01:02:23 +0600

    laravel pasport setup, laravel jetstrem setup

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\User\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/** user login Api start */
+// Route::post('/login'.[AuthController::class, 'login']);
+
+/** user login Api end */
